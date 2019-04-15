fbf667549cfdd54c72f61656f785ed306efcf857
auroratide <tf.auroratide@gmail.com>
2019-04-15 15:13:22 -0500

    Add square

--- src/components/context-api/ShapyOptions/api.js
+++ src/components/context-api/ShapyOptions/api.js
@@ -3,7 +3,8 @@ const DELAY = 500;
 export const get = () => new Promise(resolve => setTimeout(() =>
   resolve({
     shapes: [
-      '<circle cx="100" cy="100" r="50" />'
+      '<circle cx="100" cy="100" r="50" />',
+      '<path d="M50 50 L150 50 L150 150 L50 150 Z" />'
     ],
     eyes: ['happy']
   }), DELAY));
