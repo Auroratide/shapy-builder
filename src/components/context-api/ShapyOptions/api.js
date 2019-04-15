1d0b265e0d653e8ed7f242e268a44239079c96cc
auroratide <tf.auroratide@gmail.com>
2019-04-15 15:53:48 -0500

    Use relative coordinates

--- src/components/context-api/ShapyOptions/api.js
+++ src/components/context-api/ShapyOptions/api.js
@@ -4,7 +4,7 @@ export const get = () => new Promise(resolve => setTimeout(() =>
   resolve({
     shapes: [
       '<circle cx="100" cy="100" r="50" />',
-      '<path d="M50 50 L150 50 L150 150 L50 150 Z" />'
+      '<path d="M 50 50 l 100 0 l 0 100 l -100 0 Z" />'
     ],
     eyes: ['happy']
   }), DELAY));
