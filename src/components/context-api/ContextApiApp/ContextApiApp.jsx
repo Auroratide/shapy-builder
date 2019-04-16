010a27c3f3b5ccbbfe4070f85ff90a1e7c328d7c
auroratide <tf.auroratide@gmail.com>
2019-04-16 11:23:10 -0500

    Some headings

--- src/components/context-api/ContextApiApp/ContextApiApp.jsx
+++ src/components/context-api/ContextApiApp/ContextApiApp.jsx
@@ -4,6 +4,8 @@ import ShapyBuilder from '../ShapyBuilder';
 
 const ContextApiApp = () =>
   <ShapyOptions.Provider>
+    <h1>Shapy Builder!</h1>
+    <h2>Using React Context API and Hooks</h2>
     <ShapyOptions.Consumer>{({ query }) =>
       <ShapyBuilder shapes={query.shapes()} eyes={query.eyes()} />
     }</ShapyOptions.Consumer>
