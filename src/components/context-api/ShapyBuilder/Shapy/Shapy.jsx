9a82fdd4b45acbbf83bfbfc3af70b6f2907c3042
auroratide <tf.auroratide@gmail.com>
2019-04-16 16:12:33 -0500

    Cute idle animation on the shapy

--- src/components/context-api/ShapyBuilder/Shapy/Shapy.jsx
+++ src/components/context-api/ShapyBuilder/Shapy/Shapy.jsx
@@ -4,7 +4,7 @@ import styles from './style';
 
 const Shapy = ({ shape, eyes, color }) =>
   <div className={styles.shapy}>
-    <SvgString fill={color} string={shape} scale={1.3333} />
+    <SvgString className={styles.shape} fill={color} string={shape} scale={1.3333} />
     <SvgString className={styles.eyes} string={eyes} scale={1.3333} />
   </div>;
 
