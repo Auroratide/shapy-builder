73d1b5ebfd8d9dba5104e7411196ef3448489452
auroratide <tf.auroratide@gmail.com>
2019-04-17 11:29:11 -0500

    Begin adding routes so we can include more kinds of implementations

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ContextApiApp from '../context-api/ContextApiApp';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './normalize.css';
 import './style';
 
 const App = () =>
-  <ContextApiApp />;
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/context-api" component={ContextApiApp} />
+      <Route path="*" component={ContextApiApp} />
+    </Switch>
+  </BrowserRouter>;
 
 export default App;
