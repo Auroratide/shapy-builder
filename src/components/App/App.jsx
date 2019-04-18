a69d146021ebfb414f8dd3a35b91bd477b93486c
auroratide <tf.auroratide@gmail.com>
2019-04-18 15:17:07 -0500

    Added a nav bar for navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import ContextApiApp from '../context-api/ContextApiApp';
 import MobxApp from '../mobx/MobxApp';
 import ReduxApp from '../redux/ReduxApp';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './normalize.css';
-import './style';
+import styles from './style';
 
 const App = () =>
-  <Switch>
-    <Route exact path="/context-api" component={ContextApiApp} />
-    <Route exact path="/mobx" component={MobxApp} />
-    <Route exact path="/redux" component={ReduxApp} />
-    <Route path="*" component={ContextApiApp} />
-  </Switch>;
+  <React.Fragment>
+    <header className={styles.header}>
+      <nav>
+        <Link className={styles.link} to="/context-api">Context API</Link>
+        <Link className={styles.link} to="/mobx">MobX</Link>
+        <Link className={styles.link} to="/redux">Redux</Link>
+      </nav>
+    </header>
+    <Switch>
+      <Route exact path="/context-api" component={ContextApiApp} />
+      <Route exact path="/mobx" component={MobxApp} />
+      <Route exact path="/redux" component={ReduxApp} />
+      <Route path="*" component={ContextApiApp} />
+    </Switch>
+  </React.Fragment>;
 
 export default App;
