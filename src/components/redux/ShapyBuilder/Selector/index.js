1abf94a5fce6e35262735c900720bcf9fa562d1e
auroratide <tf.auroratide@gmail.com>
2019-04-19 10:20:21 -0500

    Remove residual mobx state from the redux folder

--- src/components/redux/ShapyBuilder/Selector/index.js
+++ src/components/redux/ShapyBuilder/Selector/index.js
@@ -1,8 +1,1 @@
-import { observer } from 'mobx-react';
-import State from './state';
-import Selector from './Selector';
-
-const component = observer(Selector);
-component.State = State;
-
-export default component;
+export { default } from './Selector';
